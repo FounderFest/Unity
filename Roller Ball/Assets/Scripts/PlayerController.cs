f06a2f250ae7383ebd95d806496adc59b1a502dd
Carlos Linares <cilp2912@gmail.com>
2016-01-24 13:42:53 -0600

    Juego terminado

--- Roller Ball/Assets/Scripts/PlayerController.cs
+++ Roller Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
 
+    private int count;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -21,5 +26,21 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Desactivamos el objeto en caso de que tenga un tag 'PickUp'
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
